d2066542080078730451555d407a05f6abb11444
satojkovic <satojkovic@gmail.com>
2016-03-18 06:42:27 +0900

    Download and extract the notMNIST dataset

--- tensorflow/notmnist.py
+++ tensorflow/notmnist.py
@@ -1,3 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import tarfile
+from IPython.display import display, Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+from six.moves.urllib.request import urlretrieve
+from six.moves import cPickle as pickle
+
+
+url = 'http://yaroslavvb.com/upload/notMNIST/'
+num_classes = 10
+
+
+def maybe_download(filename, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print 'Found and verified', filename
+    else:
+        raise Exception(
+            'Failed to verify:' + url + filename + '. Can you get to it with a browser?')
+    return filename
+
+
+def extract(filename):
+    tar = tarfile.open(filename)
+    root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+    if os.path.exists(root):
+        print('Already extracted for %s' % root)
+        return [os.path.join(root, dir) for dir in os.listdir(root)]
+    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    sys.stdout.flush()
+    tar.extractall()
+    tar.close()
+    data_folders = [
+        os.path.join(root, d) for d in sorted(os.listdir(root))
+        if d != '.DS_Store'
+    ]
+    if len(data_folders) != num_classes:
+        raise Exception(
+            'Expected %d folders, one per class. Found %d instead.' % (
+                num_classes, len(data_folders)))
+    print(data_folders)
+    return data_folders
+
+
+def main():
+    train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+    test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
+
+    train_folders = extract(train_filename)
+    test_folders = extract(test_filename)
+
+if __name__ == '__main__':
+    main()
