32a411535f838946cde84bc08bcc44f20264eb15
satojkovic <satojkovic@gmail.com>
2016-03-19 23:17:38 +0900

    Add load method

--- tensorflow/notmnist.py
+++ tensorflow/notmnist.py
@@ -15,6 +15,8 @@ from six.moves import cPickle as pickle
 
 url = 'http://yaroslavvb.com/upload/notMNIST/'
 num_classes = 10
+image_size = 28  # Pixel width and height.
+pixel_depth = 255.0  # Number of levels per pixel.
 
 
 def maybe_download(filename, expected_bytes):
@@ -52,12 +54,54 @@ def extract(filename):
     return data_folders
 
 
+def load(data_folders, min_num_images, max_num_images):
+    dataset = np.ndarray(
+        shape=(max_num_images, image_size, image_size), dtype=np.float32)
+    labels = np.ndarray(shape=(max_num_images), dtype=np.int32)
+    label_index = 0
+    image_index = 0
+
+    for folder in data_folders:
+        print(folder)
+        for image in os.listdir(folder):
+            if image_index >= max_num_images:
+                raise Exception('More images than expected: %d >= %d' % (
+                    image_index, max_num_images))
+            image_file = os.path.join(folder, image)
+            try:
+                image_data = (ndimage.imread(image_file).astype(float) -
+                              pixel_depth / 2) / pixel_depth
+                if image_data.shape != (image_size, image_size):
+                    raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+                dataset[image_index, :, :] = image_data
+                labels[image_index] = label_index
+                image_index += 1
+            except IOError as e:
+                print('Could not read:', image_file, ':', e,
+                      '- it\'s ok, skipping.')
+        label_index += 1
+    num_images = image_index
+    dataset = dataset[0:num_images, :, :]
+    labels = labels[0:num_images]
+    if num_images < min_num_images:
+        raise Exception('Many fewer images than expected: %d < %d' % (
+            num_images, min_num_images))
+    print('Full dataset tensor:', dataset.shape)
+    print('Mean:', np.mean(dataset))
+    print('Standard deviation:', np.std(dataset))
+    print('Labels:', labels.shape)
+    return dataset, labels
+
+
 def main():
     train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
     test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
 
     train_folders = extract(train_filename)
     test_folders = extract(test_filename)
+
+    train_dataset, train_labels = load(train_folders, 450000, 550000)
+    test_dataset, test_labels = load(test_folders, 18000, 20000)
 
 if __name__ == '__main__':
     main()
