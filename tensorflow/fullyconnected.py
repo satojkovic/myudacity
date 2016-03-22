357d6c8c24d1816fd6b8f1dbf68939ea6583069d
satojkovic <satojkovic@gmail.com>
2016-03-23 08:05:49 +0900

    Reformat dataset and labels

--- tensorflow/fullyconnected.py
+++ tensorflow/fullyconnected.py
@@ -9,12 +9,33 @@ from six.moves import range
 import uda_tf_util
 
 
+image_size = 28
+num_labels = 10
+
+
+def reformat(dataset, labels):
+    # data as a flat matrix
+    # labels as float 1-hot encodings
+    dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+    labels = (np.arange(num_labels) == labels[:, None]).astype(np.float32)
+    return dataset, labels
+
+
 def main():
     # load the notMNIST data
     train_dataset, train_labels, valid_dataset, valid_labels, \
         test_dataset, test_labels = uda_tf_util.load_pickle('notMNIST.pickle')
 
     # print details of the notMNIST dataset
+    print('Training', train_dataset.shape, train_labels.shape)
+    print('Test', test_dataset.shape, test_labels.shape)
+    print('Validation', valid_dataset.shape, valid_labels.shape)
+
+    # reformat
+    train_dataset, train_labels = reformat(train_dataset, train_labels)
+    valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+    test_dataset, test_labels = reformat(test_dataset, test_labels)
+
     print('Training', train_dataset.shape, train_labels.shape)
     print('Test', test_dataset.shape, test_labels.shape)
     print('Validation', valid_dataset.shape, valid_labels.shape)
