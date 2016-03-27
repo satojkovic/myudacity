e266c4cffd6b5a5dd8dcec0eea01656e7e053388
satojkovic <satojkovic@gmail.com>
2016-03-28 07:46:45 +0900

    Add test script

---
+++ tensorflow/regularization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import tensorflow as tf
+from six.moves import cPickle as pickle
+import uda_tf_util
+
+
+def main():
+    pass
+    
+if __name__ == '__main__':
+    main()
