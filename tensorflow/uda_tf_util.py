21b75a0c19f5870ca4ea5f5f96b560de1b4e6e65
satojkovic <satojkovic@gmail.com>
2016-04-02 22:32:09 +0900

    Flat vector or image matrix

--- tensorflow/uda_tf_util.py
+++ tensorflow/uda_tf_util.py
@@ -19,10 +19,15 @@ def load_pickle(pickle_file):
         valid_labels, test_dataset, test_labels
 
 
-def reformat(dataset, labels, num_labels):
+def reformat(dataset, labels, num_labels, is_flat=True):
     # data as a flat matrix
     # labels as float 1-hot encodings
-    data_num, flat_size = dataset.shape[0], dataset.shape[1] * dataset.shape[2]
-    dataset = dataset.reshape((-1, flat_size)).astype(np.float32)
+    data_num = dataset.shape[0]
+    if is_flat:
+        flat_size = dataset.shape[1] * dataset.shape[2]
+        dataset = dataset.reshape((-1, flat_size)).astype(np.float32)
+    else:
+        size1, size2 = dataset.shape[1], dataset.shape[2]
+        dataset = dataset.reshape((-1, size1, size2, 1)).astype(np.float32)
     labels = (np.arange(num_labels) == labels[:, None]).astype(np.float32)
     return dataset, labels
