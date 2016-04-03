b441bb3ec1ea9e41094029030ac4e834ebc0eb08
satojkovic <satojkovic@gmail.com>
2016-04-04 07:50:42 +0900

    Add sample script

---
+++ tensorflow/nlp_word2vec.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import collections
+import math
+import numpy as np
+import os
+import random
+import tensorflow as tf
+import zipfile
+from matplotlib import pylab
+from six.moves import cPickle as pickle
+from six.moves.urllib.request import urlretrieve
+from sklearn.manifold import TSNE
+
+
