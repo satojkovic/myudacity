de4d940d2276c927fe84cf2b30d97a183f860c48
satojkovic <satojkovic@gmail.com>
2016-04-05 07:56:35 +0900

    Preparing an input data

--- tensorflow/nlp_word2vec.py
+++ tensorflow/nlp_word2vec.py
@@ -13,4 +13,74 @@ from six.moves import cPickle as pickle
 from six.moves.urllib.request import urlretrieve
 from sklearn.manifold import TSNE
 
+filename = 'text8.zip'
+vocabulary_size = 50000
 
+def read_data(filename):
+    f = zipfile.ZipFile(filename)
+    for name in f.namelist():
+        return tf.compat.as_str(f.read(name)).split()
+    f.close()
+
+
+def build_dataset(words):
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(vocabulary_size - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0  # dictionary['UNK']
+            unk_count = unk_count + 1
+        data.append(index)
+    count[0][1] = unk_count
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys())) 
+    return data, count, dictionary, reverse_dictionary
+
+def generate_batch(data, data_index, batch_size, num_skips, skip_window):
+    assert batch_size % num_skips == 0
+    assert num_skips <= 2 * skip_window
+    batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+    labels = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
+    span = 2 * skip_window + 1 # [ skip_window target skip_window ]
+    buffer = collections.deque(maxlen=span)
+    for _ in range(span):
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+    for i in range(batch_size // num_skips):
+        target = skip_window  # target label at the center of the buffer
+        targets_to_avoid = [ skip_window ]
+        for j in range(num_skips):
+            while target in targets_to_avoid:
+                target = random.randint(0, span - 1)
+            targets_to_avoid.append(target)
+            batch[i * num_skips + j] = buffer[skip_window]
+            labels[i * num_skips + j, 0] = buffer[target]
+        buffer.append(data[data_index])
+    data_index = (data_index + 1) % len(data)
+    return batch, labels
+
+def main():
+    words = read_data(filename)
+    print('Data size %d' % len(words))
+
+    data, count, dictionary, reverse_dictionary = build_dataset(words)
+    print('Most common words (+UNK)', count[:5])
+    print('Sample data', data[:10])
+    del words  # Hint to reduce memory.
+
+    data_index = 0
+    batch, labels = generate_batch(data, data_index,
+                                   batch_size=8, num_skips=2, skip_window=1)
+    for i in range(8):
+        print('%d -> %d' % (batch[i], labels[i, 0]))
+        print('%s -> %s' % (reverse_dictionary[batch[i]],
+                            reverse_dictionary[labels[i, 0]]))
+
+if __name__ == '__main__':
+    main()
