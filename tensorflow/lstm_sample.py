160880ff5b65323fb73253539b4207cc8a224fb4
satojkovic <satojkovic@gmail.com>
2016-04-20 06:42:16 +0900

    Add sample script

---
+++ tensorflow/lstm_sample.py
@@ -0,0 +1,44 @@
+
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+import os
+import numpy as np
+import random
+import string
+import tensorflow as tf
+import zipfile
+from six.moves import range
+from six.moves.urllib.request import urlretrieve
+
+url = 'http://mattmahoney.net/dc/'
+
+
+def maybe_download(filename, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified %s' % filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+            'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
+
+def read_data(filename):
+    f = zipfile.ZipFile(filename)
+    for name in f.namelist():
+        return tf.compat.as_str(f.read(name))
+    f.close()
+
+
+def main():
+    filename = maybe_download('text8.zip', 31344016)
+    text = read_data(filename)
+    print('Data size %d' % len(text))
+
+
+if __name__ == '__main__':
+    main()
