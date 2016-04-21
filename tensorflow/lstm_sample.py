a5299a1e3fa28d75616d0c845f44b3cb0b9b218f
satojkovic <satojkovic@gmail.com>
2016-04-22 07:57:58 +0900

    Add BatchGenerator class

--- tensorflow/lstm_sample.py
+++ tensorflow/lstm_sample.py
@@ -13,6 +13,8 @@ from six.moves.urllib.request import urlretrieve
 url = 'http://mattmahoney.net/dc/'
 vocabulary_size = len(string.ascii_lowercase) + 1  # [a-z] + ' '
 first_letter = ord(string.ascii_lowercase[0])
+batch_size=64
+num_unrollings=10
 
 
 def maybe_download(filename, expected_bytes):
@@ -52,6 +54,53 @@ def id2char(dictid):
     else:
         return ' '
 
+# Function to generate a training batch for the LSTM model
+class BatchGenerator(object):
+    def __init__(self, text, batch_size, num_unrollings):
+        self._text = text
+        self._text_size = len(text)
+        self._batch_size = batch_size
+        self._num_unrollings = num_unrollings
+        segment = self._text_size // batch_size
+        self._cursor = [ offset * segment for offset in range(batch_size)]
+        self._last_batch = self._next_batch()
+
+    def _next_batch(self):
+        """Generate a single batch from the current cursor
+        position in the data.
+        """
+        batch = np.zeros(shape=(self._batch_size, vocabulary_size),
+                         dtype=np.float)
+        for b in range(self._batch_size):
+            batch[b, char2id(self._text[self._cursor[b]])] = 1.0
+            self._cursor[b] = (self._cursor[b] + 1) % self._text_size
+        return batch
+
+    def next(self):
+        """Generate the next array of batches from the data. The array consists of
+        the last batch of the previous array, followed by num_unrollings new ones.
+        """
+        batches = [self._last_batch]
+        for step in range(self._num_unrollings):
+            batches.append(self._next_batch())
+            self._last_batch = batches[-1]
+        return batches
+
+
+def characters(probabilities):
+    """Turn a 1-hot encoding or a probability distribution over the possible
+    characters back into its (mostl likely) character representation."""
+    return [id2char(c) for c in np.argmax(probabilities, 1)]
+
+
+def batches2string(batches):
+    """Convert a sequence of batches back into their (most likely) string
+    representation."""
+    s = [''] * batches[0].shape[0]
+    for b in batches:
+        s = [''.join(x) for x in zip(s, characters(b))]
+    return s
+
 
 def main():
     filename = maybe_download('text8.zip', 31344016)
@@ -69,6 +118,14 @@ def main():
     # map characters to vocabulary IDs and back.
     print(char2id('a'))
     print(id2char(1), id2char(26), id2char(0))
+
+    train_batches = BatchGenerator(train_text, batch_size, num_unrollings)
+    valid_batches = BatchGenerator(valid_text, 1, 1)
+
+    print(batches2string(train_batches.next()))
+    print(batches2string(train_batches.next()))
+    print(batches2string(valid_batches.next()))
+    print(batches2string(valid_batches.next()))
 
 if __name__ == '__main__':
     main()
