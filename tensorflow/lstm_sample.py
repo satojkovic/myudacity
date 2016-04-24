908234f67707d88d1cc44ca3c70a95b33f60f293
satojkovic <satojkovic@gmail.com>
2016-04-24 14:39:32 +0900

    Training a LSTM model

--- tensorflow/lstm_sample.py
+++ tensorflow/lstm_sample.py
@@ -16,6 +16,8 @@ first_letter = ord(string.ascii_lowercase[0])
 batch_size=64
 num_unrollings=10
 num_nodes = 64
+num_steps = 7001
+summary_frequency = 100
 
 
 def maybe_download(filename, expected_bytes):
@@ -251,6 +253,49 @@ def main():
         with tf.control_dependencies([saved_sample_output.assign(sample_output),
                                       saved_sample_state.assign(sample_state)]):
             sample_prediction = tf.nn.softmax(tf.nn.xw_plus_b(sample_output, w, b))
+
+    with tf.Session(graph=graph) as session:
+        tf.initialize_all_variables().run()
+        print('Initialized.')
+        mean_loss = 0
+        for step in range(num_steps):
+            batches = train_batches.next()
+            feed_dict = dict()
+            for i in range(num_unrollings + 1):
+                feed_dict[train_data[i]] = batches[i]
+            _, l, predictions, lr = session.run(
+                [optimizer, loss, train_prediction, learning_rate], feed_dict=feed_dict
+            )
+            mean_loss += l
+            if step % summary_frequency == 0:
+                if step > 0:
+                    mean_loss = mean_loss / summary_frequency
+                # The mean loss is an estimate of the loss over the last few batches.
+                print('Average loss at step %d: %f learning rate: %f' % (step, mean_loss, lr))
+            mean_loss = 0
+            labels = np.concatenate(list(batches)[1:])
+            print('Minibatch perplexity: %.2f' % float(np.exp(logprob(predictions, labels))))
+            if step % (summary_frequency * 10) == 0:
+                # Generate some samples.
+                print('=' * 80)
+                for _ in range(5):
+                    feed = sample(random_distribution())
+                    sentence = characters(feed)[0]
+                    reset_sample_state.run()
+                    for _ in range(79):
+                        prediction = sample_prediction.eval({sample_input: feed})
+                        feed = sample(prediction)
+                        sentence += characters(feed)[0]
+                    print(sentence)
+                print('=' * 80)
+            # Measure validation set perplexity.
+            reset_sample_state.run()
+            valid_logprob = 0
+            for _ in range(valid_size):
+                b = valid_batches.next()
+                predictions = sample_prediction.eval({sample_input: b[0]})
+                valid_logprob = valid_logprob + logprob(predictions, b[1])
+            print('Validation set perplexity: %.2f' % float(np.exp(valid_logprob / valid_size)))
 
 if __name__ == '__main__':
     main()
