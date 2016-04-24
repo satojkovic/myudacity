d92c9fcdc9e595b487ecaa12421b05419c1f0a86
satojkovic <satojkovic@gmail.com>
2016-04-24 13:46:52 +0900

    Generate a graph

--- tensorflow/lstm_sample.py
+++ tensorflow/lstm_sample.py
@@ -15,6 +15,7 @@ vocabulary_size = len(string.ascii_lowercase) + 1  # [a-z] + ' '
 first_letter = ord(string.ascii_lowercase[0])
 batch_size=64
 num_unrollings=10
+num_nodes = 64
 
 
 def maybe_download(filename, expected_bytes):
@@ -53,6 +54,39 @@ def id2char(dictid):
         return chr(dictid + first_letter - 1)
     else:
         return ' '
+
+
+def logprob(predictions, labels):
+    """Log-probability of the true labels in a predicted batch."""
+    predictions[predictions < 1e-10] = 1e-10
+    return np.sum(np.multiply(labels, -np.log(predictions))) / labels.shape[0]
+
+
+def sample_distribution(distribution):
+    """Sample one element from a distribution assumed to be an array of normalized
+    probabilities.
+    """
+    r = random.uniform(0, 1)
+    s = 0
+    for i in range(len(distribution)):
+        s += distribution[i]
+        if s >= r:
+            return i
+    return len(distribution) - 1
+
+
+def sample(prediction):
+    """Turn a (column) prediction into 1-hot encoded samples."""
+    p = np.zeros(shape=[1, vocabulary_size], dtype=np.float)
+    p[0, sample_distribution(prediction[0])] = 1.0
+    return p
+
+
+def random_distribution():
+    """Generate a random column of probabilities."""
+    b = np.random.uniform(0.0, 1.0, size=[1, vocabulary_size])
+    return b/np.sum(b, 1)[:,None]
+
 
 # Function to generate a training batch for the LSTM model
 class BatchGenerator(object):
@@ -126,6 +160,97 @@ def main():
     print(batches2string(train_batches.next()))
     print(batches2string(valid_batches.next()))
     print(batches2string(valid_batches.next()))
+
+    # Generate a graph
+    graph = tf.Graph()
+    with graph.as_default():
+
+        # Parameters:
+        # Input gate: input, previous output, and bias.
+        ix = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+        im = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+        ib = tf.Variable(tf.zeros([1, num_nodes]))
+        # Forget gate: input, previous output, and bias.
+        fx = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+        fm = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+        fb = tf.Variable(tf.zeros([1, num_nodes]))
+        # Memory cell: input, state and bias.
+        cx = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+        cm = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+        cb = tf.Variable(tf.zeros([1, num_nodes]))
+        # Output gate: input, previous output, and bias.
+        ox = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
+        om = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
+        ob = tf.Variable(tf.zeros([1, num_nodes]))
+        # Variables saving state across unrollings.
+        saved_output = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
+        saved_state = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
+        # Classifier weights and biases.
+        w = tf.Variable(tf.truncated_normal([num_nodes, vocabulary_size], -0.1, 0.1))
+        b = tf.Variable(tf.zeros([vocabulary_size]))
+
+        # Definition of the cell computation.
+        def lstm_cell(i, o, state):
+            """Create a LSTM cell. See e.g.: http://arxiv.org/pdf/1402.1128v1.pdf
+            Note that in this formulation, we omit the various connections between the
+            previous state and the gates."""
+            input_gate = tf.sigmoid(tf.matmul(i, ix) + tf.matmul(o, im) + ib)
+            forget_gate = tf.sigmoid(tf.matmul(i, fx) + tf.matmul(o, fm) + fb)
+            update = tf.matmul(i, cx) + tf.matmul(o, cm) + cb
+            state = forget_gate * state + input_gate * tf.tanh(update)
+            output_gate = tf.sigmoid(tf.matmul(i, ox) + tf.matmul(o, om) + ob)
+            return output_gate * tf.tanh(state), state
+
+        # Input data.
+        train_data = list()
+        for _ in range(num_unrollings + 1):
+            train_data.append(
+                tf.placeholder(tf.float32, shape=[batch_size,vocabulary_size]))
+        train_inputs = train_data[:num_unrollings]
+        train_labels = train_data[1:]  # labels are inputs shifted by one time step.
+
+        # Unrolled LSTM loop.
+        outputs = list()
+        output = saved_output
+        state = saved_state
+        for i in train_inputs:
+            output, state = lstm_cell(i, output, state)
+            outputs.append(output)
+
+        # State saving across unrollings.
+        with tf.control_dependencies([saved_output.assign(output),
+                                      saved_state.assign(state)]):
+            # Classifier.
+            logits = tf.nn.xw_plus_b(tf.concat(0, outputs), w, b)
+            loss = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(
+                    logits, tf.concat(0, train_labels)))
+
+        # Optimizer.
+        global_step = tf.Variable(0)
+        learning_rate = tf.train.exponential_decay(
+            10.0, global_step, 5000, 0.1, staircase=True)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+        gradients, v = zip(*optimizer.compute_gradients(loss))
+        gradients, _ = tf.clip_by_global_norm(gradients, 1.25)
+        optimizer = optimizer.apply_gradients(
+            zip(gradients, v), global_step=global_step)
+
+        # Predictions.
+        train_prediction = tf.nn.softmax(logits)
+
+        # Sampling and validation eval: batch 1, no unrolling.
+        sample_input = tf.placeholder(tf.float32, shape=[1, vocabulary_size])
+        saved_sample_output = tf.Variable(tf.zeros([1, num_nodes]))
+        saved_sample_state = tf.Variable(tf.zeros([1, num_nodes]))
+        reset_sample_state = tf.group(
+            saved_sample_output.assign(tf.zeros([1, num_nodes])),
+            saved_sample_state.assign(tf.zeros([1, num_nodes])))
+        sample_output, sample_state = lstm_cell(
+            sample_input, saved_sample_output, saved_sample_state)
+        with tf.control_dependencies([saved_sample_output.assign(sample_output),
+                                      saved_sample_state.assign(sample_state)]):
+            sample_prediction = tf.nn.softmax(tf.nn.xw_plus_b(sample_output, w, b))
 
 if __name__ == '__main__':
     main()
